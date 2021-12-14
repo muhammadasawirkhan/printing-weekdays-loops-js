917d8f95bacb63c098559380b72076a12470aabb
muhammadasawirkhan <95438981+muhammadasawirkhan@users.noreply.github.com>
2021-12-14 19:15:49 +0500

    Add files via upload

---
+++ printing weekdays loops js/js/main.js
@@ -0,0 +1,8 @@
+var weekdays = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+
+document.write("<h3>Printing Weekdays by For Loop</h3>");
+
+for(i = 0;i < weekdays.length;i++)
+{
+	document.write("<br/>"+weekdays[i]);
+}
